4ca3e533103ced53c95244cd3dfe1331bea34137
Paul E Davey <paul.davey@xtravirt.com>
2020-08-31 18:57:36 +0100

    Update packer_cfg.sh

--- packer_cfg.sh
+++ packer_cfg.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #-----------------------------------------------------------------
 # The spinner in this script:
-#   Copyright of KatworX© Tech. Developed by Arjun Singh Kathait
+#   Copyright of KatworXÂ© Tech. Developed by Arjun Singh Kathait
 #   and Debugged by the Stack Overflow Community
 #-----------------------------------------------------------------
 
 spinner() {
     local PROC="$1"
-    local str="${2:-'Copyright of KatworX© Tech. Developed by Arjun Singh Kathait and Debugged by the ?Stack Overflow Community?'}"
+    local str="${2:-'Copyright of KatworXÂ© Tech. Developed by Arjun Singh Kathait and Debugged by the ?Stack Overflow Community?'}"
     local delay="0.1"
     tput civis  # hide cursor
-    #printf "\033[1;34m"
+    printf "\033[1;34m"
     while [ -d /proc/$PROC ]; do
         printf '\033[s\033[u[ / ] %s\033[u' "$str"; sleep "$delay"
-        printf '\033[s\033[u[ — ] %s\033[u' "$str"; sleep "$delay"
+        printf '\033[s\033[u[ â€” ] %s\033[u' "$str"; sleep "$delay"
         printf '\033[s\033[u[ \ ] %s\033[u' "$str"; sleep "$delay"
         printf '\033[s\033[u[ | ] %s\033[u' "$str"; sleep "$delay"
     done
@@ -25,42 +25,42 @@ clear
 echo "AutomationPro Packer Install & Config Script"
 echo "--------------------------------------------"
 
-sleep 1 & spinner $! "Install wget, nano, git & unzip"
+sleep 3 & spinner $! "Install wget, nano, git & unzip"
 sudo yum install wget nano unzip -y > /dev/null 2>&1
 
-sleep 1 & spinner $! "Updating OS and components"
+sleep 3 & spinner $! "Updating OS and components"
 sudo yum update -y > /dev/null 2>&1
 
-sleep 1 & spinner $! "Create the /usr/bin/hashicorp folder"
+sleep 3 & spinner $! "Create the /usr/bin/hashicorp folder"
 mkdir -p /usr/local/bin/hashicorp > /dev/null 2>&1
 
-sleep 1 & spinner $! "Create the /usr/bin/hashicorp/packer folder"
+sleep 3 & spinner $! "Create the /usr/bin/hashicorp/packer folder"
 mkdir -p /usr/local/bin/hashicorp/packer > /dev/null 2>&1
 
-sleep 1 & spinner $! "Navigate to directory"
+sleep 3 & spinner $! "Navigate to directory"
 cd /usr/local/bin/hashicorp/packer > /dev/null 2>&1
 
-sleep 1 & spinner $! "Download Packer [version 1.6.2]"
+sleep 3 & spinner $! "Download Packer [version 1.6.2]"
 wget https://releases.hashicorp.com/packer/1.6.2/packer_1.6.2_linux_amd64.zip > /dev/null 2>&1
 
-sleep 1 & spinner $! "Extract Packer from zip file"
+sleep 3 & spinner $! "Extract Packer from zip file"
 unzip packer_1.6.2_linux_amd64.zip > /dev/null 2>&1
 
-sleep 1 & spinner $! "Removing zip file"
+sleep 3 & spinner $! "Removing zip file"
 rm -f packer_1.6.2_linux_amd64.zip > /dev/null 2>&1
 
-sleep 1 & spinner $! "Confirm correct version of Hashicorp Packer is installed"
+sleep 3 & spinner $! "Confirm correct version of Hashicorp Packer is installed"
 packerversion=$(/usr/local/bin/hashicorp/packer/packer --version) > /dev/null 2>&1
 
 if [ "$packerversion" == "1.6.2" ]; then
-	sleep 1 & spinner $! "Packer [version 1.6.2] install. They call it a Royale with cheese..."
+	sleep 3 & spinner $! "Packer [version 1.6.2] install. They call it a Royale with cheese..."
 else
     echo -e "...Houston, we have a problem.." \e[101mLight red
     echo -e "Packer version is $packerversion, expected version 1.6.2" \e[101mLight red
     exit 1
 fi
 
-sleep 1 & spinner $! "Cloning Automationpro public packer github repo"
+sleep 3 & spinner $! "Cloning Automationpro public packer github repo"
 cd /usr/local/bin/hashicorp/packer > /dev/null 2>&1
 git clone https://github.com/pauledavey/AutomationPro_Packer.git > /dev/null 2>&1
 echo "Operation(s) Complete. You're welcome. Roadhouse.."
